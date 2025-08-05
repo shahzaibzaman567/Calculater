9ecdbdd9ffac2c95e610b951a549918a2bc7bb69
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-08-04 18:16:02 -0700

    add js logics

---
+++ app.js
@@ -0,0 +1,20 @@
+let btn=document.querySelectorAll(".btn");
+
+for(let i=0;i<btn.length ;i++   ){
+
+let butt=   document.querySelector(".btn");
+
+btn[i].addEventListener("click",()=>{
+
+    let input=document.querySelector(".numbers");
+    let String="";
+    input.value+=btn[i].innerHTML;
+    let find= 22;   
+//is ko kal findvkaronga
+
+
+
+})
+
+
+}
